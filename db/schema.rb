4e2689d209fe6bd41b1c02f8c3dddb8a494184b5
Isaac Ahern <Isaac.m.ahern@gmail.com>
2021-12-13 21:09:13 -0600

    create completed method

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_12_211930) do
+ActiveRecord::Schema.define(version: 2021_12_14_023126) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2021_12_12_211930) do
     t.integer "project_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.datetime "completed_at"
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
